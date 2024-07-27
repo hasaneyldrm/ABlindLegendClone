417d1fe39036a7368f1528a62f38f700e7bb5657
Hasan Emir Yıldırım <hasanyldrm@hotmail.de>
2024-07-27 20:24:15 +0300

    init commit

---
+++ Assets/Scripts/CheckPointManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CheckPointManager : MonoBehaviour
+{
+    public AudioClip checkpointSound;
+    public AudioSource audioSource;
+    public Collider2D checkpointCollider;
+
+    void Start()
+    {
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+        audioSource.playOnAwake = false; // Sesin başlangıçta çalmaması için
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (checkpointSound != null)
+            {
+                audioSource.PlayOneShot(checkpointSound);
+            }
+            Debug.Log("Checkpoint reached!");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+    }
+}
